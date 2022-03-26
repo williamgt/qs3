6aa2c87c865690f24d1e2cb1b052d99e2c6e6303
William G. Tresselt <williagt@stud.ntnu.no>
2022-03-26 16:45:35 +0100

    initial commit

---
+++ backend/src/main/java/no/ntnu/idatt2105/gr13/qs3backend/Qs3BackendApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2105.gr13.qs3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qs3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qs3BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/no/ntnu/idatt2105/gr13/qs3backend/Qs3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2105.gr13.qs3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qs3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
