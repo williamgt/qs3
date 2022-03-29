160ec9b142f05f2dd6788e6c4be9d67c95b1bf48
ofplarsen <ofplarsen@gmail.com>
2022-03-29 11:28:58 +0200

    Whoops wrong branch. Started on course-admin-view'

---
+++ frontend/src/components/admin/course/CourseAdmin.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "CourseAdmin"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/admin/CourseAdminView.vue
@@ -0,0 +1,15 @@
+<template>
+<course-admin></course-admin>
+</template>
+
+<script>
+import CourseAdmin from "@/components/admin/course/CourseAdmin";
+export default {
+  name: "CourseAdminView",
+  components: {CourseAdmin}
+}
+</script>
+
+<style scoped>
+
+</style>
