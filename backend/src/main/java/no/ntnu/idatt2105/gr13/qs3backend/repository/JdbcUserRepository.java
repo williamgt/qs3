1d356f1772c506d4fdeb9f6c1c9f6cb7d5b96d03
ofplarsen <ofplarsen@gmail.com>
2022-03-30 18:11:04 +0200

    Merge fix

--- backend/src/main/java/no/ntnu/idatt2105/gr13/qs3backend/repository/JdbcUserRepository.java
+++ backend/src/main/java/no/ntnu/idatt2105/gr13/qs3backend/repository/JdbcUserRepository.java
@@ -3,6 +3,7 @@ package no.ntnu.idatt2105.gr13.qs3backend.repository;
 import no.ntnu.idatt2105.gr13.qs3backend.model.user.User;
 import no.ntnu.idatt2105.gr13.qs3backend.model.user.UserDB;
 import no.ntnu.idatt2105.gr13.qs3backend.model.user.UserPerson;
+import no.ntnu.idatt2105.gr13.qs3backend.model.user.UserPersonAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.IncorrectResultSetColumnCountException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -29,11 +30,30 @@ public class JdbcUserRepository implements UserRepository {
     public UserPerson findById(long id) {
         try{
             String query = "SELECT * from User, Person where User.personId = Person.id and User.personId = ?";
-            UserDB user = jdbcTemplate.queryForObject(query, (rs, rowNum) ->
-                    new UserDB(
+            UserPerson user = jdbcTemplate.queryForObject(query, (rs, rowNum) ->
+                    new UserPerson(
                             rs.getString("email"),
-                            rs.getString("password"),
-                            rs.getInt("personId")
+                            rs.getString("firstname"),
+                            rs.getString("lastname"),
+                            rs.getInt("id")
+                    ), id);
+            return user;
+        }catch (IncorrectResultSetColumnCountException e){
+            return null;
+        }
+    }
+
+    @Override
+    public UserPersonAll findByIdAdmin(long id) {
+        try{
+            String query = "SELECT * from User, Person where User.personId = Person.id and User.personId = ?";
+            UserPersonAll user = jdbcTemplate.queryForObject(query, (rs, rowNum) ->
+                    new UserPersonAll(
+                            rs.getString("email"),
+                            rs.getString("firstname"),
+                            rs.getString("lastname"),
+                            rs.getInt("id"),
+                            rs.getString("password")
                     ), id);
             return user;
         }catch (IncorrectResultSetColumnCountException e){
