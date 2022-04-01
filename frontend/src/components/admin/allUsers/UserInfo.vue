1ea732fabd2cb15a86bcf4fe50c0d95c8db9517d
ofplarsen <ofplarsen@gmail.com>
2022-04-01 10:04:10 +0200

    Commit before merge

---
+++ frontend/src/components/admin/allUsers/UserInfo.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "UserInfo"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/admin/UserInfoView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "UserInfoView"
+}
+</script>
+
+<style scoped>
+
+</style>
